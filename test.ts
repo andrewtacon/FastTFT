9559e06235de4f1145a26a381d96130253830e8d
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-19 19:55:22 +1000

    xx

--- test.ts
+++ test.ts
@@ -1,9 +1,12 @@
-// tests go here; this will not be compiled when this package is used as an extension.
-FastTFT.api.init()
-FastTFT.api.clear(0)
-FastTFT.api.fillRect(10, 10, 40, 40, 63488)
-FastTFT.api.fillRect(70, 10, 40, 40, 2016)
-FastTFT.api.createFramebuffer()
-FastTFT.api.clearFramebuffer(0)
-FastTFT.api.fillFramebufferRect(30, 70, 68, 30, 65504)
-FastTFT.api.show()
+// // tests go here; this will not be compiled when this package is used as an extension.
+// FastTFT.init()
+// FastTFT.clear(0)
+// FastTFT.fillRect(10, 10, 40, 40, 63488)
+// FastTFT.fillRect(70, 10, 40, 40, 2016)
+// FastTFT.createFramebuffer()
+// FastTFT.clearFramebuffer(0)
+// FastTFT.fillFramebufferRect(30, 70, 68, 30, 65504)
+// FastTFT.show()
+
+
+FastTFT.bananas(2);
