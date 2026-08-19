a74f499c619aaa767daa0b74a0eb14a5e4eeeb9b
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-19 20:14:59 +1000

    1

--- test.ts
+++ test.ts
@@ -1,12 +1,10 @@
-// // tests go here; this will not be compiled when this package is used as an extension.
-// FastTFT.init()
-// FastTFT.clear(0)
-// FastTFT.fillRect(10, 10, 40, 40, 63488)
-// FastTFT.fillRect(70, 10, 40, 40, 2016)
-// FastTFT.createFramebuffer()
-// FastTFT.clearFramebuffer(0)
-// FastTFT.fillFramebufferRect(30, 70, 68, 30, 65504)
-// FastTFT.show()
+// tests go here; this will not be compiled when this package is used as an extension.
+FastTFT.init()
+FastTFT.clear(0)
+FastTFT.fillRect(10, 10, 40, 40, 63488)
+FastTFT.fillRect(70, 10, 40, 40, 2016)
+FastTFT.createFramebuffer()
+FastTFT.clearFramebuffer(0)
+FastTFT.fillFramebufferRect(30, 70, 68, 30, 65504)
+FastTFT.show()
 
-
-FastTFT.bananas();
